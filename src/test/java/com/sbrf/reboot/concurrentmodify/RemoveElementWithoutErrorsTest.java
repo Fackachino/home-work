d9bf284bc76344d7c435f226015aba4843ac2d4d
Igor Samoylov <samoylov11gor@gmail.com>
2022-01-28 16:49:16 +0300

    commit: lesson-8   - Переделал удаление элемента из коллекции так, чтобы не было выброшено исключение.

--- src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
+++ src/test/java/com/sbrf/reboot/concurrentmodify/RemoveElementWithoutErrorsTest.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
+import java.util.Iterator;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class RemoveElementWithoutErrorsTest {
@@ -31,6 +33,43 @@ public class RemoveElementWithoutErrorsTest {
 
     @Test
     public void successRemoveElementWithoutErrors() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+
+        assertDoesNotThrow(() -> {
+            List<Integer> removeList = new ArrayList<>();
+
+            for (Integer integer : list) {
+                removeList.add(1);
+            }
+            list.removeAll(removeList);
+
+        });
+
+    }
+
+    @Test
+    public void successRemoveElementWithoutErrors2() {
+        List<Integer> list = new ArrayList() {{
+            add(1);
+            add(2);
+            add(3);
+        }};
+
+        assertDoesNotThrow(() -> {
+        Iterator<Integer> integerIterator = list.iterator();
+        int i = 0;
+        while(integerIterator.hasNext()){
+            integerIterator.next();
+            if(i == 1){
+                integerIterator.remove();
+            }
+            i++;
+        }
+        });
 
     }
 
