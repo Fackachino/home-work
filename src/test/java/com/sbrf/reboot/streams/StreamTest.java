9147405d1be461395f8d298dfdc6a1209505e954
Igor Samoylov <samoylov11gor@gmail.com>
2022-02-09 16:15:56 +0300

    lesson 11  - добился успешного выполнения всех тестов, согласно ТЗ

--- src/test/java/com/sbrf/reboot/streams/StreamTest.java
+++ src/test/java/com/sbrf/reboot/streams/StreamTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -21,7 +23,9 @@ public class StreamTest {
 
         List<Integer> expectedIntegers = Arrays.asList(3, 6, 8, 9);
 
-        List<Integer> actualIntegers = null; //add code here
+        List<Integer> actualIntegers = integers.stream()
+                .sorted()
+                .collect(Collectors.toList());
 
         assertEquals(expectedIntegers, actualIntegers);
     }
@@ -36,7 +40,10 @@ public class StreamTest {
 
         List<Integer> expectedIntegers = Arrays.asList(6, 8);
 
-        List<Integer> actualIntegers = null; //add code here
+        List<Integer> actualIntegers = integers.stream()
+                .filter(integer -> integer % 2 == 0)
+                .sorted()
+                .collect(Collectors.toList());
 
         assertEquals(expectedIntegers, actualIntegers);
 
@@ -71,7 +78,10 @@ public class StreamTest {
 
         );
 
-        List<Book> actualBooks = null; //add code here
+        List<Book> actualBooks = books.stream()
+                .filter(book -> book.author.equals("Maria"))
+                .sorted(Comparator.comparing(book -> book.price))
+                .collect(Collectors.toList());
 
         assertEquals(expectedBooks, actualBooks);
 
@@ -88,10 +98,18 @@ public class StreamTest {
 
         List<String> expectedContracts = Arrays.asList("M-NCC-1-CH", "M-NCC-2-US", "M-NCC-3-NH");
 
-        List<String> actualContracts = null; //add code here
+        List<String> actualContracts = contracts.stream()
+                .map(string -> string = "M-" + string)
+                .collect(Collectors.toList());
 
         assertEquals(expectedContracts, actualContracts);
 
     }
+
+    /*
+     *
+     *
+     *
+     */
 
 }
